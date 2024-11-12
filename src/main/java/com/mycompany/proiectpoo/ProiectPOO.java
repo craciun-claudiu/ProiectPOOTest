f9a20b574761bfcb250ceedfe17ff53dbb960399
cc642 <cc642@WIN-3.CTI.UGAL.RO>
2024-11-12 20:48:13 +0200

    Modificare

--- src/main/java/com/mycompany/proiectpoo/ProiectPOO.java
+++ src/main/java/com/mycompany/proiectpoo/ProiectPOO.java
@@ -11,6 +11,7 @@ package com.mycompany.proiectpoo;
 public class ProiectPOO {
 
     public static void main(String[] args) {
-        System.out.println("Am schimbat codul a doua oara");
+        System.out.println("Ceva");
+        System.out.println("Am schimbat codul a treia oara");
     }
 }
