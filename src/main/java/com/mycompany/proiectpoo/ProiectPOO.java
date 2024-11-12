48dde7674baa5166bd389225148020f848a74c9d
cc642 <cc642@WIN-3.CTI.UGAL.RO>
2024-11-12 20:13:02 +0200

    Test1

---
+++ src/main/java/com/mycompany/proiectpoo/ProiectPOO.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.proiectpoo;
+
+/**
+ *
+ * @author cc642
+ */
+public class ProiectPOO {
+
+    public static void main(String[] args) {
+        System.out.println("Test1");
+    }
+}
